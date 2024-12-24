6a8789ade77aef045d568dc8f908584eaa44304f
Jared <jaredlll08@gmail.com>
2024-12-24 00:54:30 -0700

    Initial commit

---
+++ src/main/java/com/blamejared/exnihilocoloratus/ExNihiloColoratus.java
@@ -0,0 +1,15 @@
+package com.blamejared.colorstages;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(ExNihiloColoratus.MODID)
+public class ExNihiloColoratus {
+
+    public static final String MODID = "exnihilocoloratus";
+    public static final Logger LOG = LogUtils.getLogger();
+
+    public ExNihiloColoratus() {
+    }
+}
